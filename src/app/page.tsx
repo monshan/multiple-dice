df27e2e0f8b3fe4991689409a23b095d65567f79
Marika Shanahan <marikashanahan@gmail.com>
2023-11-16 23:27:24 -0700

    Fully working

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,12 +5,12 @@ import DiceLayout from "@/components/DiceLayout"
 import { useState } from "react";
 
 export default function Home() {
-  const [numberOfDice, setNumberOfDice] = useState(1);
+  const [diceValues, setDiceValues] = useState([1]);
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-evenly gap-7 p-24">
-      <NumberOfDice setNumberOfDice={setNumberOfDice} />
-      <DiceLayout numberOfDice={numberOfDice} />
+    <main className="flex min-h-screen flex-col items-center justify-evenly gap-8 p-24">
+      <NumberOfDice setDiceValues={setDiceValues} />
+      <DiceLayout diceValues={diceValues} />
     </main>
   )
 }
--- src/components/NumberOfDice.tsx
+++ src/components/NumberOfDice.tsx
@@ -1,22 +1,27 @@
-"use client"
-
-import { useState } from "react";
+import { ChangeEventHandler, useState } from "react";
+import generateRandomNumber from "@/utils/generateRandomNumber";
 
 interface NumberOfDiceProps {
-    setNumberOfDice: (numberOfDice: number) => void;
+    setDiceValues: React.Dispatch<React.SetStateAction<Array<number>>>
 }
 
-export default function NumberOfDice({ setNumberOfDice }: NumberOfDiceProps) {
-    const [formData, setFormData] = useState(1)
+export default function NumberOfDice({ setDiceValues }: NumberOfDiceProps) {
+    const [formData, setFormData] = useState<Array<number>>([]);
+    const numberOfDiceHandler: ChangeEventHandler<HTMLInputElement> = (e) => {
+        const newArray: Array<number> = [];
+        for (let i = 0; i < e.target.valueAsNumber; i++) {
+            newArray[i] = generateRandomNumber(1, 6);
+        }
+        setFormData(newArray);
+    };
 
     return (
-        <form 
-            className="flex flex-col gap-4"
-            onSubmit={(e) => setNumberOfDice(formData)}
-        >
+        <div className="flex flex-col gap-4">
             <h1>Number of Dice</h1>
-            <input type="number" min="1" max="99" defaultValue={1} className="border-solid border-2 border-stone-500 p-1" onChange={(e) => setFormData(e.target.valueAsNumber)} />
-            <button type="submit" className="bg-stone-200">Roll</button>
-        </form>
+            <input type="number" min="1" max="99" defaultValue={1} className="border-solid border-2 border-stone-500 p-1" 
+                onChange={numberOfDiceHandler} 
+            />
+            <button onClick={() => setDiceValues(formData)} className="bg-stone-200">Roll</button>
+        </div>
     )
 }
